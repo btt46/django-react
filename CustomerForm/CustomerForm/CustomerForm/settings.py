0abfe0bc698ff1ab695b91b07adc608342f18198
btthanh <40657914+btt46@users.noreply.github.com>
2021-04-07 16:19:51 +0900

    hide security key

--- CustomerForm/CustomerForm/CustomerForm/settings.py
+++ CustomerForm/CustomerForm/CustomerForm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hc+&^2*%=gydr50g=*wtd3!i#k$%=x95c-o%6&4n&bvqy8io5j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
